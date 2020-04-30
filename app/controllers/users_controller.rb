82839a0ab7d7ffd6ced82a7abc2a273aada393e8
Theophile-Kango <fadhili.kango@gmail.com>
2020-04-30 16:41:09 +0200

    Add edit user to our project

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
+    def index
+        @users = User.all
+    end
+
     def new
         @user = User.new
     end
@@ -11,6 +15,22 @@ class UsersController < ApplicationController
         else
             render :new
         end
+    end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            redirect_to @user
+        else
+            render 'edit'
+        end
+    end
+    def show
+        @user = User.find(params[:id])
     end
 
     private
