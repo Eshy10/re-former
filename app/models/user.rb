239ab26e475cb393bf7a98d8ef38a74b8febdade
modupe <adewalebose15@gmail.com>
2020-04-30 16:24:13 +0100

    update form for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-    validates :username, :email, presence: true, uniqueness: true
-    validates :password, presence: true, length: { minimum: 6 }
+  validates :username, :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
 end
